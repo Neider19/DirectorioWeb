b48c50d6e20a3cfb737bdbb5225dee42ffcdc70a
Neider19 <neiderureche952@gmail.com>
2024-10-02 21:03:39 -0500

    validacion de inicio

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+function validarInicio(){
+
+    let username = "Neider"
+    let password = "123";
+    
+    if (username == document.getElementById("usuario").value && password == document.getElementById("password").value){
+        
+        window.location.href="./inicio.html";
+        alert("Sesion Iniciada");
+    }
+    else {
+        alert("Usuario y/o contraseña incorrectos");
+    }
+
+}
---
+++ JS/registrar.js
@@ -0,0 +1,9 @@
+function validarRegistro(){
+
+    let username =  document.getElementById("usuario").value;
+    let tel =  document.getElementById("telefono").value;
+    let email =  document.getElementById("correo").value;
+    let contrasena =  document.getElementById("password").value;
+   
+        Window.location.href = "index.html";
+}
